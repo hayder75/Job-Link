160fe0d3d259fe43d868c05bf7ffbf6463ca41b7
hayder75 <hayderaman75@gmail.com>
2025-01-01 17:22:15 +0300

    part ui created

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+}
+  from 'react-native'
+import { useRouter } from 'expo-router'
+import styles from './welcome.style'
+import { icons, SIZES, } from '../../../constants'
+
+const jobType = ['Full-time', 'Part-time', 'Contract-', '-time', '-Type']
+
+const Welcome = () => {
+  const router = useRouter();
+
+    const [activeJobType,setactiveJobType]=useState('Full-time')
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Gabe</Text>
+        <Text style={styles.welcomeMessage}>Find the perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput}
+            value=''
+            onChange={() => { }}
+            placeholder='what are you looking for ' />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+          <Image source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobType}
+          renderItem={({item}) => (
+            <TouchableOpacity style={styles.tab(activeJobType , item)}
+              onPress={()=>{
+                setactiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+              
+           >
+              <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor = {item=> item}
+          contentContainerStyle={{columnGap:SIZES.small}}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+
+        />
+
+      </View>
+
+    </View>
+  )
+}
+
+export default Welcome
---
+++ index.js
@@ -0,0 +1,9 @@
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+const App = () => {
+  const ctx = require.context('./app');
+  return <ExpoRoot context={ctx} />;
+};
+
+registerRootComponent(App);
